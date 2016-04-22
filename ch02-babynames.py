70ce004db287fffbdd8bebe25c5729a68f7ebbc9
Gloria Guy <totallygloria@gmail.com>
2016-04-21 17:35:28 -0700

    Added functionality to select only the top 1000 most common names for a given year, sort them into the most common boy and girl names, and plot trends for a given name over the data set. Note that the plot is the absolute number of names, not percentage vs births, so birthrate change affect the plot.

--- ch02-babynames.py
+++ ch02-babynames.py
@@ -8,6 +8,7 @@ This code snippet comes from Ch 02 of Python for Data Analysis by Wes McKinney
 """
 
 import pandas as pd
+import numpy as np
 
 names1880 = pd.read_csv('ch02/names/yob1880.txt', names=['name', 'sex', 'births'])
 
@@ -16,7 +17,7 @@ names1880 = pd.read_csv('ch02/names/yob1880.txt', names=['name', 'sex', 'births'
 
 # For approximate total number of births in a year:
 
-#print names1880.groupby('sex')['births'].sum()
+# print names1880.groupby('sex')['births'].sum()
 
 
 # Since the data is separated into files by year, the first thing is to combine
@@ -38,13 +39,66 @@ for year in years:
 
 names = pd.concat(pieces, ignore_index=True)
 
-#print names[:25]
+# print names[:25]
 
 
 # Now we can aggregate the data at the year and sex level using groupby or pivot_table
 
 total_births = names.pivot_table('births', index='year', columns='sex', aggfunc=sum)
 
-print total_births.tail()
+# print total_births.tail()
 
 total_births.plot(title='Total births by sex and year')
+
+
+def add_prop(group):
+    # adds the proportion of births for a given name (a name / all births)
+    # Integer division floors, so cast to float
+    births = group.births.astype(float)
+    group['prop %'] = (births / births.sum()) * 100
+
+    return group
+
+names = names.groupby(['year', 'sex']).apply(add_prop)
+
+# print names[:10]
+
+# Do a sanity check to make sure the total is sufficiently close to 100 (all names)
+# This gives an error in pycharm, but returns True in ipython.
+
+# np.allclose(names.groupby(['year', 'sex']).prop.sum(), 100)
+
+
+def get_top1000(group):
+    return group.sort_values(by='births', ascending=False)[:1000]
+
+
+grouped = names.groupby(['year', 'sex'])
+top1000 = grouped.apply(get_top1000)
+top1000.index = np.arange(len(top1000))
+
+# print top1000
+
+'''
+# the do-it-yourself version would be
+
+pieces = []
+for year, group in names.groupby(['year','sex']):
+    pieces.append(group.sort_values(by='births', ascending=False)[:1000]
+top1000 = pd.concat(pieces, ignore_index=True)
+'''
+
+# To do more interesting analysis, let's sort the top1000 into boys and girls
+
+boys = top1000[top1000.sex == 'M']
+girls = top1000[top1000.sex == 'F']
+
+total_births = top1000.pivot_table('births', index='year', columns='name', aggfunc=sum)
+
+# print total_births.info()
+
+# This information can now be plotted for a handful of names using DataFrame's plot method
+
+subset = total_births[['John', 'Eric', 'Mary', 'Gloria']]
+
+subset.plot(subplots=True, figsize=(12,10), grid=False, title="Number of births per year for 4 names")
